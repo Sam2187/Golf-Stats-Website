6ca5f3a4f35ed4867e8aa2381a60b7cc0a98fa1a
Sam Scholes <88086653+Sam2187@users.noreply.github.com>
2021-12-12 09:55:51 +0000

    Add files via upload

---
+++ ryder.py
@@ -0,0 +1,10 @@
+
+Answer = input("what number")
+if input == 2:
+    print ("Correct")
+elif Answer == 3:
+    print ("Wrong")
+elif Answer == 4:
+    print ("Wrong")
+else:
+    print ("Try again") 
